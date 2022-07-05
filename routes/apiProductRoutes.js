8770e1307a4beb898f7b88f368edeedcf7d5a7e5
santio89 <santiolais1989@gmail.com>
2022-07-05 01:15:57 -0300

    routes/controllers - products/carts

--- routes/apiProductRoutes.js
+++ routes/apiProductRoutes.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
-const {getAllProducts, getProductById, postProduct, putProduct, deleteProductById} = require("../controllers/apiProductController");;
+const {getAllProducts, getProductById, postProduct, putProduct, deleteProductById} = require("../controllers/apiProductController");
+const ADMIN = true;
 
+const checkAdmin = (req,res,next)=>{
+    if (ADMIN === true){
+        next();
+    } else{
+        res.json({error: -1, descripcion: `Ruta '${req.route.path}' Método '${req.route.stack[0].method}' - No Autorizada`})
+    }
+}
 
 /* ruteo */
 router.get("/", getAllProducts)
 router.get("/:id", getProductById)
-router.post("/", postProduct)
-router.put("/:id", putProduct)
-router.delete("/:id", deleteProductById)
+router.post("/", checkAdmin, postProduct)
+router.put("/:id", checkAdmin, putProduct)
+router.delete("/:id", checkAdmin, deleteProductById)
 
 module.exports = router;
