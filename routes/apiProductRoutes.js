b535266942f4168a9d924ecc84c7075548198165
santio89 <santiolais1989@gmail.com>
2022-07-06 09:19:34 -0300

    small fix

--- routes/apiProductRoutes.js
+++ routes/apiProductRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {getAllProducts, getProductById, postProduct, putProduct, deleteProductById} = require("../controllers/apiProductController");
+const {getProductById, postProduct, putProduct, deleteProductById} = require("../controllers/apiProductController");
 const checkAdminUtil = require("../utils/checkAdmin")
 
 const ADMIN = true;
 const checkAdmin = checkAdminUtil(ADMIN);
 
 /* ruteo */
-/* id? -> si existe id, busca id. si no existe id, busca todos */
 router.get("/:id?", getProductById) 
 router.post("/", checkAdmin, postProduct)
 router.put("/:id", checkAdmin, putProduct)
